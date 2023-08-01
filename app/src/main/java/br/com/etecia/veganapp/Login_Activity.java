44d964fb74cdc217b4e335e725d8d7dcbc89fb57
luismiguel172 <luis.goncalves68@etec.sp.gov.br>
2023-08-01 20:22:23 -0300

    Commit

--- app/src/main/java/br/com/etecia/veganapp/Login_Activity.java
+++ app/src/main/java/br/com/etecia/veganapp/Login_Activity.java
@@ -1,13 +1,11 @@
 package br.com.etecia.veganapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Handler;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
@@ -16,27 +14,17 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import br.com.etecia.veganapp.databinding.SplashLayoutBinding;
+import br.com.etecia.veganapp.databinding.LoginLayoutBinding;
 
-public class Splash_Activity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private SplashLayoutBinding binding;
+    private LoginLayoutBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_layout);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(getApplicationContext(),
-                        Login_Activity.class));
-                finish();
-            }
-        },3000);
 
 
-            }
-        }
+    }
+}
